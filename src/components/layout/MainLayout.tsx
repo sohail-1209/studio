ab5b9d4df39e57a1ce43097ae2a3a8cfdd8f642a
Arulla Tv <arullatv@gmail.com>
2025-06-12 03:06:52 +0000

    now i can see side navbar in feed page

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -43,27 +43,13 @@ function LayoutContent({ children }: MainLayoutProps) {
 
   return (
     <div className="flex min-h-screen"> {/* Root flex container */}
-      <UISidebar
-        style={{
-          backgroundColor: 'red', // Direct inline style for testing
-          width: '256px',        // Direct inline style for testing
-          borderRight: '4px solid black',
-          position: 'sticky',
-          top: '0',
-          height: '100vh',
-          zIndex: 100, // High z-index
-          flexShrink: 0,
-        }}
-        // We keep some classes for flex behavior if needed, but critical visual styles are inline
-        className="flex flex-col"
-      >
+      <UISidebar> {/* Removed inline style and className prop */}
         <AppSidebar />
       </UISidebar>
-      <SidebarInset> {/* This is the div with !bg-blue-500 and flex-1 */}
+      <SidebarInset>
         {/* The direct child of SidebarInset gets the padding and max-width for content */}
         <div
           className="w-full max-w-5xl mx-auto p-4 sm:p-6 lg:p-8"
-          // The blue background is now directly on SidebarInset from ui/sidebar.tsx
         >
           {children}
         </div>
